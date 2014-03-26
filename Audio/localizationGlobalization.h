c9106cecb9d04cbf42a06fa33a1e626a0bf07e09
adryan200o <adryan_200o@yahoo.com>
2014-03-26 04:02:06 +0200

    #2 Globalization function
    AM create functia de globalizare

--- Audio/localizationGlobalization.h
+++ Audio/localizationGlobalization.h
@@ -18,5 +18,11 @@ void grow_w(window &w , int count, bool &g);
 /*	Create window limits from seed	*/
 window create_window_from_seed (int seed);
 
+/*	Search for signal s in localization windows	*/
+void search_for_s_in_loc(short *data ,int s , window * loc ,int nr_loc , window * rezult , int &contor);
+
 /*	Compute localization windows for given data	*/
 void compute_localization (short *data,int count, int channels , double * mean , double * st_dev , window * loc ,  int & nr_loc);
+
+/*	Compute localization windows for given data	*/
+void compute_globalization (short *data,int count, int channels, window * loc , int & nr_loc, window * glob , int & nr_glob);
